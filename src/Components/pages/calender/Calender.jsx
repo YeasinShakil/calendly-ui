911da0c2f76fde64157a6e410857705964d9cfec
Yeasin_Shakil <shakilrock1998@gmail.com>
2023-03-14 16:05:30 +0600

    responsive lay out update

--- src/Components/pages/calender/Calender.jsx
+++ src/Components/pages/calender/Calender.jsx
@@ -5,7 +5,7 @@ import CalenderRight from './CalenderRight';
 const Calender = () => {
     return (
        
-            <div className=' h-full md:w-[60vw] md:h-[80vh] mx-auto mt-20 rounded-lg mb-5  shadow-lg border-t-2 grid md:grid-cols-2 relative'>
+            <div className=' h-full md:w-[60vw] md:h-[80vh] mx-auto mt-20 rounded-lg mb-5  shadow-lg border-t-2 grid md:grid-cols-1 lg:grid-cols-2 relative'>
             <div className='border-r border-black/50'>
                 <CalenderRight></CalenderRight>
             </div>
--- src/Components/pages/calender/CalenderRight.jsx
+++ src/Components/pages/calender/CalenderRight.jsx
@@ -8,13 +8,13 @@ const CalenderRight = () => {
             <div className=' w-10 h-10 rounded-full border flex justify-center items-center text-2xl text-blue-500 cursor-pointer hover:bg-blue-200'>
                 <AiOutlineArrowLeft className=' font-bold' />
             </div>
-            <p className='mt-10 font-medium text-gray-400 text-center md:text-left '>Bitbytesoft Oy</p>
-            <h3 className='mt-3 text-2xl font-medium text-center md:text-left '>30 Minute Meeting</h3>
-            <div className='mt-8 flex items-center text-gray-400'>
+            <p className='mt-10 font-medium text-gray-400 text-center lg:text-left '>Bitbytesoft Oy</p>
+            <h3 className='mt-3 text-2xl font-medium text-center lg:text-left '>30 Minute Meeting</h3>
+            <div className='mt-8 flex items-center justify-center lg:flex-1 text-gray-400'>
                 <AiFillClockCircle />
                 <p className=' ml-2 font-medium'>30 min</p>
             </div>
-            <div className='cursor-pointer absolute bottom-4 text-center right-50'>
+            <div className='cursor-pointer absolute sm:bottom-4 sm:left-5 left-[35%] bottom-[-15px] text-center right-50'>
                 <p className=' text-blue-500'>Cookie settings</p>
             </div>
         </div>
